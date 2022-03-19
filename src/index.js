db0e8002277acc110313c01f85e964304aa6a43e
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-19 21:38:47 +0200

    Feature: add front folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-
+import path from 'path';
 const app = express();
+app.use(express.static(path.join(__dirname, '../front')));
 
 app.get('/', (req, res) => {
-	res.send('Hello world')
+	res.sendFile(path.join(__dirname, '../front', 'index.html'));
 });
+
 
 app.listen(3000, () => {
 	console.log('server started');
