dae2b2d4ef365f65abd985665d43daca2558d299
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-20 23:07:37 +0200

    Feature: [DB-User] generate and decode jwt token

--- src/classes/dbModels/User.js
+++ src/classes/dbModels/User.js
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 import Model from '@classes/dbModels/Abstract/Model';
 import dbClient from '@lib/dbClient';
-import { USER_PASSWORD_SALT_ROUNDS } from '@config/env';
+import { USER_PASSWORD_SALT_ROUNDS, USER_TOKEN_EXPIRES_HOURS, USER_TOKEN_SECRET_KEY } from '@config/env';
 
 class User extends Model {
 	
@@ -27,6 +28,30 @@ class User extends Model {
 				}
 			});
 		});
+	}
+	
+	static async createToken(data) {
+		return new Promise((resolve, reject) => {
+			jwt.sign(data, USER_TOKEN_SECRET_KEY, { expiresIn: `${USER_TOKEN_EXPIRES_HOURS}h` }, (err, token) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(token);
+				}
+			});
+		});
+	}
+	
+	static async decodeToken(token) {
+		return new Promise((resolve, reject) => {
+			jwt.verify(token, USER_TOKEN_SECRET_KEY, (err, decoded) => {
+				if(err) {
+					reject(err)
+				} else {
+					resolve(decoded)
+				}
+			});
+		})
 	}
 }
 
