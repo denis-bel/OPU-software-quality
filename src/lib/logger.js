9e026f620a4639c2e16abb6aa761f769f579a2c1
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-23 09:20:01 +0200

    Feature: add logger

---
+++ src/lib/logger.js
@@ -0,0 +1,36 @@
+import winston from 'winston';
+const colorizer = winston.format.colorize();
+
+const alignColorsAndTime = winston.format.combine(
+	winston.format.timestamp({
+		format: 'YY-MM-DD HH:MM:SS'
+	}),
+	winston.format.printf(msg =>
+		colorizer.colorize(msg.level, `[${msg.timestamp}][${msg.level}]: ${msg.message}`)
+	)
+);
+
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.combine(
+		winston.format.timestamp({
+			format: 'YY-MM-DD HH:MM:SS'
+		}),
+		winston.format.printf(msg =>
+			`[${msg.timestamp}][${msg.level}] ${msg.message}`
+		)
+	),
+	transports: [
+		new winston.transports.File({ filename: 'error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'combined.log' }),
+		new winston.transports.File({ filename: 'warn.log', level: 'warn' })
+	]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(new winston.transports.Console({
+		format: alignColorsAndTime
+	}));
+}
+
+export default logger
