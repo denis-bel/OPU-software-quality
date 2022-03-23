ba3298c8783d85cf7fbc1389e7c00e7077ea87b1
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-23 10:44:05 +0200

    Feature: log volume

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -21,9 +21,9 @@ const logger = winston.createLogger({
 		)
 	),
 	transports: [
-		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'combined.log' }),
-		new winston.transports.File({ filename: 'warn.log', level: 'warn' })
+		new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'logs/combined.log' }),
+		new winston.transports.File({ filename: 'logs/warn.log', level: 'warn' })
 	]
 });
 
