7a1da7be19ccc7163f3063a3b252bbb89e2d9e6e
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-26 21:23:39 +0200

    Fix: [API] moddleware wrapper

--- src/lib/middlewareWrapper.js
+++ src/lib/middlewareWrapper.js
@@ -4,7 +4,7 @@ import { HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
 export default middleware => (req, res, next) => {
 	return middleware(req, res, next)
 		.catch(error => {
-			logger.error(error);
+			logger.error(error.message);
 			const expressError = new Error('Internal Server Error');
 			expressError.status = HTTP_CODE_SERVER_ERROR;
 			next(expressError);
