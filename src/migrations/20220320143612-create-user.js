3121a3337248cd326f24e127c48e66af42c04d7c
Денис Белый <denisbel@MacBook-Air-Denis.local>
2022-03-28 22:58:54 +0300

    Feature: [DB-migration-user] login constraint

--- src/migrations/20220320143612-create-user.js
+++ src/migrations/20220320143612-create-user.js
@@ -33,6 +33,10 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+    /* eslint-disable no-useless-escape */
+    await queryInterface.sequelize.query(`
+      ALTER TABLE ${tableName} ADD constraint proper_login CHECK (login ~* '^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d.-]{0,19}$');
+      `);
   },
   async down(queryInterface) {
     await queryInterface.dropTable(tableName);
