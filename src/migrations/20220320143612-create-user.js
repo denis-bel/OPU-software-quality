0206cde3166c39fd94b77aa3d79a730fde10fdd7
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-10 18:16:04 +0300

    Add default value to timestamps in user migration

--- src/migrations/20220320143612-create-user.js
+++ src/migrations/20220320143612-create-user.js
@@ -1,44 +1,46 @@
 const { USER_ROLE_ADMIN, USER_ROLE_USER, USER_ROLE_SUPER_ADMIN } = require('@constants/User');
 
-const tableName = 'users'
+const tableName = 'users';
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(tableName, {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      login: {
-        type: Sequelize.STRING(50),
-        unique: true,
-        allowNull: false
-      },
-      password: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      },
-      role: {
-        type: Sequelize.ENUM(USER_ROLE_ADMIN, USER_ROLE_USER, USER_ROLE_SUPER_ADMIN),
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-    /* eslint-disable no-useless-escape */
-    await queryInterface.sequelize.query(`
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(tableName, {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			login: {
+				type: Sequelize.STRING(50),
+				unique: true,
+				allowNull: false
+			},
+			password: {
+				type: Sequelize.STRING(100),
+				allowNull: false
+			},
+			role: {
+				type: Sequelize.ENUM(USER_ROLE_ADMIN, USER_ROLE_USER, USER_ROLE_SUPER_ADMIN),
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			}
+		});
+		/* eslint-disable no-useless-escape */
+		await queryInterface.sequelize.query(`
       ALTER TABLE ${tableName} ADD constraint proper_login CHECK (login ~* '^[a-z0-9_-]{3,16}$');
       `);
-  },
-  async down(queryInterface) {
-    await queryInterface.dropTable(tableName);
-  }
+	},
+	async down(queryInterface) {
+		await queryInterface.dropTable(tableName);
+	}
 };
