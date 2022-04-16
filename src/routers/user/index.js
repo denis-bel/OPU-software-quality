63848dd278eb664d8938ed73e7e86dd097aea88d
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-16 22:47:38 +0300

    Update role access for get all users route

--- src/routers/user/index.js
+++ src/routers/user/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import roleAccess from '@middlewares/roleAccess';
 import authorizeUser from '@middlewares/authorizeUser';
-import { USER_ROLE_SUPER_ADMIN } from '@constants/User';
+import { USER_ROLE_ADMIN, USER_ROLE_SUPER_ADMIN } from '@constants/User';
 import getAllMiddlewares from '@routers/user/getAll';
 import updateMiddlewares from '@routers/user/update';
 import deleteMiddlewares from '@routers/user/delete';
@@ -10,9 +10,10 @@ import createMiddlewares from '@routers/user/create';
 const router = express.Router();
 
 router.use(authorizeUser);
-router.use(roleAccess([USER_ROLE_SUPER_ADMIN]));
 
-router.get('/all', ...getAllMiddlewares);
+router.get('/all', roleAccess([USER_ROLE_SUPER_ADMIN, USER_ROLE_ADMIN]), ...getAllMiddlewares);
+
+router.use(roleAccess([USER_ROLE_SUPER_ADMIN]));
 router.put('/', ...updateMiddlewares);
 router.delete('/:id', ...deleteMiddlewares);
 router.post('/', ...createMiddlewares);
