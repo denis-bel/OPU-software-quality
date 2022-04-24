56e142fdf44cd4b645f75673521605f6a140a45f
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-24 17:03:34 +0300

    Add migration for tools

---
+++ src/migrations/20220424140201-create-tools.js
@@ -0,0 +1,31 @@
+const tableName = 'tools';
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(tableName, {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			name: {
+				type: Sequelize.STRING(50),
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			}
+		});
+	},
+	async down(queryInterface) {
+		await queryInterface.dropTable(tableName);
+	}
+};
