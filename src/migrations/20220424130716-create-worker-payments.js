7dca88fbc6421b8e00f03dd5ec7084ba21b04d12
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-24 16:13:49 +0300

    Add migration for worker payments

---
+++ src/migrations/20220424130716-create-worker-payments.js
@@ -0,0 +1,43 @@
+const tableName = 'worker_payments';
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(tableName, {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			sum: {
+				type: Sequelize.FLOAT,
+				allowNull: false
+			},
+			workerId: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'workers',
+					key: 'id'
+				}
+			},
+			date: {
+				type: Sequelize.DATE,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('NOW')
+			}
+		});
+	},
+	async down(queryInterface) {
+		await queryInterface.dropTable(tableName);
+	}
+};
