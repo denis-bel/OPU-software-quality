78d244af4490992044827463c1adf9b6d99da1cf
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-24 21:04:07 +0300

    Add seeder for road objects

---
+++ src/seeders/20220424180030-test-road-objects.js
@@ -0,0 +1,19 @@
+const tableName = 'road-objects';
+
+module.exports = {
+	up: async queryInterface =>
+		await queryInterface.bulkInsert(tableName, [{
+			name: 'Object 1',
+			number: 1,
+			address: '4363 Cinnamon Lane'
+		}, {
+			name: 'Object 2',
+			number: 2,
+			address: '1378 Progress Way'
+		}, {
+			name: 'Object 3',
+			number: 3,
+			address: '1320 American Drive'
+		}]),
+	down: queryInterface => queryInterface.bulkDelete(tableName, null, {})
+};
