7646be27c3866edc344f57495e24d8de03a2d602
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-04-24 21:17:50 +0300

    Add work types seeder

---
+++ src/seeders/20220424181702-test-work-types.js
@@ -0,0 +1,13 @@
+const tableName = 'work-types';
+
+module.exports = {
+	up: async queryInterface =>
+		await queryInterface.bulkInsert(tableName, [{
+			name: 'Work type 1'
+		}, {
+			name: 'Work type 2'
+		}, {
+			name: 'Work type 3'
+		}]),
+	down: queryInterface => queryInterface.bulkDelete(tableName, null, {})
+};
