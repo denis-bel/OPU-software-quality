fdfc7ad355e329485ebb09b98ed91ef45cb94409
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-01 15:11:07 +0300

    Add method to get usedTransport with name

--- src/classes/dbModels/UsedTransport.js
+++ src/classes/dbModels/UsedTransport.js
@@ -1,8 +1,18 @@
 import Model from '@classes/dbModels/Abstract/Model';
 import dbClient from '@lib/dbClient';
+import Query from '@classes/Query';
 
 class UsedTransport extends Model {
-
+	static async findWithName(filter) {
+		const query = new Query();
+		query.add(`
+			SELECT used_transports.id, count , "transportId", used_transports."createdAt", used_transports."updatedAt", name FROM used_transports
+			INNER JOIN transports ON used_transports."transportId" = transports.id
+			`);
+		this._addWhere(query, filter.where);
+		const { rows } = await this._dbClient.query(query.query, query.values);
+		return rows;
+	}
 }
 
 UsedTransport.initialize({ tableName: 'used_transports', dbClient, withTimeStamps: true });
--- src/routers/usedTransport/getAll.js
+++ src/routers/usedTransport/getAll.js
@@ -3,7 +3,7 @@ import UsedTransport from '@classes/dbModels/UsedTransport';
 
 async function getAll(req, res) {
 	const { activityId } = req.query;
-	const usedTransports = await UsedTransport.find({ where: { activityId } });
+	const usedTransports = await UsedTransport.findWithName({ where: { activityId } });
 	res.json({ usedTransports });
 }
 
