7b9244a87e553f55efa9ebb203af2120f137eaf8
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-01 15:54:08 +0300

    Add getting all data from activity "get all" route

--- src/classes/dbModels/Activity.js
+++ src/classes/dbModels/Activity.js
@@ -1,8 +1,22 @@
 import Model from '@classes/dbModels/Abstract/Model';
 import dbClient from '@lib/dbClient';
+import Query from '@classes/Query';
 
 class Activity extends Model {
-
+	static async findWithInclude(filter) {
+		const query = new Query();
+		query.add(`
+			SELECT "startDate", "finishDate", "brigadeId", "roadObjectId", activities.id, brigades."name" AS "brigadeName",
+			road_objects.name AS "roadObjectName", brigades.number AS "brigadeNumber", address AS "roadObjectAddress",
+			road_objects."number" AS "roadObjectNumber"
+			FROM activities
+			INNER JOIN brigades ON "brigadeId" = brigades.id
+			INNER JOIN road_objects ON "roadObjectId" = road_objects.id
+			`);
+		this._addWhere(query, filter.where);
+		const { rows } = await this._dbClient.query(query.query, query.values);
+		return rows;
+	}
 }
 
 Activity.initialize({ tableName: 'activities', dbClient, withTimeStamps: true });
--- src/routers/activity/getAll.js
+++ src/routers/activity/getAll.js
@@ -3,7 +3,7 @@ import Activity from '@classes/dbModels/Activity';
 
 async function getAll(req, res) {
 	const { brigadeId, roadObjectId } = req.query;
-	const activities = await Activity.find({ where: { brigadeId, roadObjectId } });
+	const activities = await Activity.findWithInclude({ where: { brigadeId, roadObjectId } });
 	res.json({ activities });
 }
 
