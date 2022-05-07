c5a49ce3cc4618ad3f01c1822f7f56784beb3983
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-07 15:12:12 +0300

    Add file with pure sql requests

---
+++ src/requests.sql
@@ -0,0 +1,21 @@
+-- 1
+-- Список імен робітників бригади номера Х
+SELECT "fullName"
+FROM brigades INNER JOIN employees ON brigades.id = employees."brigadeId"
+WHERE "number" = 1
+
+-- Сумма сплат робітника з ПІБ Х
+SELECT sum FROM employee_payments
+INNER JOIN employees ON "employeeId"=employees.id
+WHERE "fullName"='Joshua K. Gunn'
+
+-- 2
+-- Список працівників ПІБ яких починається з літери 'J'
+SELECT * FROM employees
+WHERE "fullName" LIKE 'J%'
+
+-- Список бригад назва яких починається з літери 'B'
+SELECT * FROM brigades
+WHERE "name" LIKE 'B%'
+
+-- 3
