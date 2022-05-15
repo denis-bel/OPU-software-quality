f1d1dbeac1f63600c838c3b1618ca14f2fa29399
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-15 19:15:45 +0300

    Add route to update brigade

---
+++ src/routers/brigade/update.js
@@ -0,0 +1,15 @@
+import { HTTP_CODE_BAD_REQUEST } from '@constants/httpCode';
+import Brigade from '@classes/dbModels/Brigade';
+
+async function update(req, res) {
+	const { id, ...data } = req.body;
+	if (!id) {
+		return res.status(HTTP_CODE_BAD_REQUEST).json({
+			message: 'Missing id'
+		});
+	}
+	const brigade = await Brigade.updateById(data, id);
+	res.send({ brigade });
+}
+
+export default [update];
