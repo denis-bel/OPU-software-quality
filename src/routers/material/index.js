a4a0f870b20a336b611628fdc49ce4aa113530c3
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-16 21:20:10 +0300

    Add route to update material

--- src/routers/material/index.js
+++ src/routers/material/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authorizeUser from '@middlewares/authorizeUser';
 import getAllMiddlewares from './getAll';
 import createAllMiddlewares from './create';
+import updateMiddlewares from './update';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.use(authorizeUser);
 
 router.get('/all', ...getAllMiddlewares);
 router.post('/', ...createAllMiddlewares);
+router.put('/', ...updateMiddlewares);
 
 export default router;
