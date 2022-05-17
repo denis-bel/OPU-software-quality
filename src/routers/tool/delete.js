0b9218a12b9dec2e3d5fe0fce07e53226293627f
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-17 07:25:19 +0300

    Add route to delete tool

---
+++ src/routers/tool/delete.js
@@ -0,0 +1,25 @@
+import middlewareWrapper from '@lib/middlewareWrapper';
+import { HTTP_CODE_NOT_FOUND, HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
+import Tool from '@classes/dbModels/Tool';
+
+async function deleteMaterial(req, res) {
+	const { id } = req.params;
+	const tool = await Tool.findById(id);
+	if (!tool) {
+		return res.status(HTTP_CODE_NOT_FOUND).json({
+			message: 'Tool not found'
+		});
+	}
+	const isDeleted = await Tool.deleteById(id);
+	if (isDeleted) {
+		return res.send({
+			message: 'Tool deleted successfully'
+		});
+	} else {
+		return res.status(HTTP_CODE_SERVER_ERROR).send({
+			message: 'Error deleting brigade'
+		});
+	}
+}
+
+export default [middlewareWrapper(deleteMaterial)];
