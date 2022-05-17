84c0b1f860c1c7eaa1af8725ac87740145de8b18
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-17 19:59:52 +0300

    Add route to delete transport

---
+++ src/routers/transport/delete.js
@@ -0,0 +1,25 @@
+import middlewareWrapper from '@lib/middlewareWrapper';
+import { HTTP_CODE_NOT_FOUND, HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
+import Transport from '@classes/dbModels/Transport';
+
+async function deleteTransport(req, res) {
+	const { id } = req.params;
+	const transport = await Transport.findById(id);
+	if (!transport) {
+		return res.status(HTTP_CODE_NOT_FOUND).json({
+			message: 'Transport not found'
+		});
+	}
+	const isDeleted = await Transport.deleteById(id);
+	if (isDeleted) {
+		return res.send({
+			message: 'Transport deleted successfully'
+		});
+	} else {
+		return res.status(HTTP_CODE_SERVER_ERROR).send({
+			message: 'Error deleting brigade'
+		});
+	}
+}
+
+export default [middlewareWrapper(deleteTransport)];
