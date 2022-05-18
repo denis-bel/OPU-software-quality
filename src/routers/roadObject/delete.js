5e047275f8bfb5c86d7840a0e28cb7118bb44a72
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-18 07:58:05 +0300

    Add route to delete road object

---
+++ src/routers/roadObject/delete.js
@@ -0,0 +1,25 @@
+import middlewareWrapper from '@lib/middlewareWrapper';
+import { HTTP_CODE_NOT_FOUND, HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
+import RoadObject from '@classes/dbModels/RoadObject';
+
+async function deleteRoadObject(req, res) {
+	const { id } = req.params;
+	const roadObject = await RoadObject.findById(id);
+	if (!roadObject) {
+		return res.status(HTTP_CODE_NOT_FOUND).json({
+			message: 'Road object not found'
+		});
+	}
+	const isDeleted = await RoadObject.deleteById(id);
+	if (isDeleted) {
+		return res.send({
+			message: 'Road object was deleted successfully'
+		});
+	} else {
+		return res.status(HTTP_CODE_SERVER_ERROR).send({
+			message: 'Error deleting road object'
+		});
+	}
+}
+
+export default [middlewareWrapper(deleteRoadObject)];
