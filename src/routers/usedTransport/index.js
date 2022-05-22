1c43b04d9e981151d2902bc58ace11a1164dabee
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-22 09:44:47 +0300

    Add route to update used transport

--- src/routers/usedTransport/index.js
+++ src/routers/usedTransport/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authorizeUser from '@middlewares/authorizeUser';
 import getAllMiddlewares from './getAll';
 import createMiddlewares from './create';
+import updateMiddleWares from './update';
 
 const router = express.Router();
 
 router.use(authorizeUser);
 router.get('/all', ...getAllMiddlewares);
 router.post('/', ...createMiddlewares);
+router.put('/', ...updateMiddleWares);
 
 export default router;
