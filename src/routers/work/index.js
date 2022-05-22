ad1bbee649c1c3e68f2d468e980646c8085d3cf2
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-22 14:47:21 +0300

    Add route to update work

--- src/routers/work/index.js
+++ src/routers/work/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import authorizeUser from '@middlewares/authorizeUser';
 import getAllMiddlewares from './getAll';
 import createMiddlewares from './create';
+import updateMiddlewares from './update';
 
 const router = express.Router();
 
 router.use(authorizeUser);
 router.get('/all', ...getAllMiddlewares);
 router.post('/', ...createMiddlewares);
+router.put('/', ...updateMiddlewares);
 
 export default router;
