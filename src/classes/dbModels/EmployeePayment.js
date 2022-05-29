5cc34c53213b501291f646a19ff98443f9c2483b
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-29 17:23:15 +0300

    Add fullName to EmployeePayment findInRange()

--- src/classes/dbModels/EmployeePayment.js
+++ src/classes/dbModels/EmployeePayment.js
@@ -5,9 +5,14 @@ import Query from '@classes/Query';
 class EmployeePayment extends Model {
 	static async findInRange(minSum, maxSum) {
 		const query = new Query();
-		query.add(`SELECT *
-               FROM ${this._tableName}
-               WHERE sum BETWEEN ${query.nextIndex} AND ${query.nextIndex}`, [minSum, maxSum]);
+		query.add(
+			`SELECT ep.id,
+              ep.sum,
+              ep."date",
+              e."fullName"
+       FROM employee_payments ep
+                INNER JOIN employees e ON e.id = ep."employeeId"
+       WHERE sum BETWEEN ${query.nextIndex} AND ${query.nextIndex}`, [minSum, maxSum]);
 		const { rows } = await this._dbClient.query(query.query, query.values);
 		return rows;
 	}
