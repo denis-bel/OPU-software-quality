bda0d96619cc26c37431268247f2d6c499029c6d
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-30 07:21:07 +0300

    Add method to Brigade to get all brigades with employee count

--- src/classes/dbModels/Brigade.js
+++ src/classes/dbModels/Brigade.js
@@ -2,7 +2,15 @@ import Model from '@classes/dbModels/Abstract/Model';
 import dbClient from '@lib/dbClient';
 
 class Brigade extends Model {
-
+	static async findAllWithEmployeeCount() {
+		const query = `SELECT b.id, b."name", b."number", COUNT(e.id) AS "employeeCount"
+                   FROM brigades b
+                            LEFT JOIN employees e
+                                      ON e."brigadeId" = b.id
+                   GROUP BY b.id;`;
+		const { rows } = await this._dbClient.query(query);
+		return rows;
+	}
 }
 
 Brigade.initialize({ tableName: 'brigades', dbClient, withTimeStamps: true });
--- src/routers/brigade/getAll.js
+++ src/routers/brigade/getAll.js
@@ -2,7 +2,7 @@ import Brigade from '@classes/dbModels/Brigade';
 import middlewareWrapper from '@lib/middlewareWrapper';
 
 async function getAll(req, res) {
-	const brigades = await Brigade.findAll();
+	const brigades = await Brigade.findAllWithEmployeeCount();
 	res.send({ brigades });
 }
 
