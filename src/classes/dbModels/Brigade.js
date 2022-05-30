18aa869e7b2b3670adad0b5433c1941ae8d0a9c6
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-05-30 07:41:32 +0300

    Add extra info to brigades in get all

--- src/classes/dbModels/Brigade.js
+++ src/classes/dbModels/Brigade.js
@@ -3,11 +3,27 @@ import dbClient from '@lib/dbClient';
 
 class Brigade extends Model {
 	static async findAllWithEmployeeCount() {
-		const query = `SELECT b.id, b."name", b."number", COUNT(e.id) AS "employeeCount"
+		const query = `SELECT b.id,
+                          b."name",
+                          b."number",
+                          COUNT(e.id) AS "employeeCount",
+                          "info"
                    FROM brigades b
                             LEFT JOIN employees e
-                                      ON e."brigadeId" = b.id
-                   GROUP BY b.id;`;
+                                      ON
+                                          e."brigadeId" = b.id
+                            LEFT JOIN (SELECT brigades.id,
+                                              'Maximum activities' AS "info"
+                                       FROM brigades
+                                                INNER JOIN activities ON
+                                           activities."brigadeId" = brigades.id
+                                       GROUP BY brigades.id
+                                       HAVING COUNT(*) >= ALL (SELECT COUNT(*)
+                                                               FROM activities
+                                                               GROUP BY "brigadeId")) AS A
+                                      ON
+                                          a.id = b.id
+                   GROUP BY b.id, info;`;
 		const { rows } = await this._dbClient.query(query);
 		return rows;
 	}
