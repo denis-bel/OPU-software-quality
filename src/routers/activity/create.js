9ba15e1c1e043f6bc39b72b54f8b3649d56b93e9
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-06-04 20:46:18 +0300

    Feature: log activity routes to db

--- src/routers/activity/create.js
+++ src/routers/activity/create.js
@@ -1,9 +1,12 @@
 import middlewareWrapper from '@lib/middlewareWrapper';
 import Activity from '@classes/dbModels/Activity';
+import UserLog from '@classes/dbModels/UserLog';
 
 async function create(req, res) {
 	const { startDate, finishDate, brigadeId, roadObjectId } = req.body;
 	const activity = await Activity.create({ startDate, finishDate, brigadeId, roadObjectId });
+	const { user } = req;
+	await UserLog.create({ userId: user.id, action: 'Create activity' });
 	res.json({ activity });
 }
 
--- src/routers/activity/delete.js
+++ src/routers/activity/delete.js
@@ -1,6 +1,7 @@
 import middlewareWrapper from '@lib/middlewareWrapper';
 import { HTTP_CODE_NOT_FOUND, HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
 import Activity from '@classes/dbModels/Activity';
+import UserLog from '@classes/dbModels/UserLog';
 
 async function deleteActivity(req, res) {
 	const { id } = req.params;
@@ -12,6 +13,8 @@ async function deleteActivity(req, res) {
 	}
 	const isDeleted = await Activity.deleteById(id);
 	if (isDeleted) {
+		const { user } = req;
+		await UserLog.create({ userId: user.id, action: 'Delete activity' });
 		return res.send({
 			message: 'Activity was deleted successfully'
 		});
