bd4c17a72a5000dd4802a8bc433541ece6bff20f
denis-bel <denisbel@MacBook-Air-Denis.local>
2022-09-10 08:00:46 +0300

    refactor(error-handler): extract methods

--- refactor.md
+++ refactor.md
@@ -238,3 +238,24 @@ class ActivityService {
   }
 }
 ```
+
+## 8
+Extract methods
+
+```javascript
+import { HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
+
+export default (error, req, res, next) => {
+	const { message, status } = error;
+	res.status(getStatus(status)).json({ message: getMessage(message) });
+	next();
+}
+
+function getStatus(status) {
+	return status || HTTP_CODE_SERVER_ERROR;
+}
+
+function getMessage(message) {
+	return message || 'Internal server error';
+}
+```
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,15 @@
 import { HTTP_CODE_SERVER_ERROR } from '@constants/httpCode';
 
 export default (error, req, res, next) => {
-	let { message, status } = error;
-	if (!status) {
-		status = HTTP_CODE_SERVER_ERROR;
-		message = 'Internal server error';
-	}
-	res.status(status).json({ message });
+	const { message, status } = error;
+	res.status(getStatus(status)).json({ message: getMessage(message) });
 	next();
+}
+
+function getStatus(status) {
+	return status || HTTP_CODE_SERVER_ERROR;
+}
+
+function getMessage(message) {
+	return message || 'Internal server error';
 }
